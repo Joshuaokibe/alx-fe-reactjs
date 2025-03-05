0ce9901392e663a8bb570064a3ec0e66abf91961
emeke <joshuaokibe@gmail.com>
2025-03-05 11:12:29 -0800

    my accoount

--- recipe-sharing-app/src/components/EditRecipeForm.js
+++ recipe-sharing-app/src/components/EditRecipeForm.js
@@ -1,5 +1,5 @@
 import { useState } from 'react';
-import { useRecipeStore } from '../store/recipeStore';
+import { useRecipeStore } from './recipeStore';
 import { useParams, useNavigate } from 'react-router-dom';
 
 const EditRecipeForm = () => {
