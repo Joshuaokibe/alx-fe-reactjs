e5707a0cc016cdcdea028aa14c60e15e362f7c1d
emeke <joshuaokibe@gmail.com>
2025-03-05 10:24:56 -0800

    my task

--- recipe-sharing-app/src/recipeStore.js
+++ recipe-sharing-app/src/recipeStore.js
@@ -1,9 +1,19 @@
 import { create } from 'zustand';
 
-const useRecipeStore = create((set) => ({
-  recipes: [], // Initial state: empty array of recipes
-  addRecipe: (newRecipe) => set((state) => ({ recipes: [...state.recipes, newRecipe] })),
-  setRecipes: (recipes) => set({ recipes }),
-}));
+export const useRecipeStore = create((set) => ({
+  recipes: [],
+  
+  addRecipe: (recipe) => set((state) => ({
+    recipes: [...state.recipes, { id: Date.now(), ...recipe }]
+  })),
 
-export default useRecipeStore;
+  updateRecipe: (id, updatedRecipe) => set((state) => ({
+    recipes: state.recipes.map(recipe =>
+      recipe.id === id ? { ...recipe, ...updatedRecipe } : recipe
+    )
+  })),
+
+  deleteRecipe: (id) => set((state) => ({
+    recipes: state.recipes.filter(recipe => recipe.id !== id)
+  })),
+}));
